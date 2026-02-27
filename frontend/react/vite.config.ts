84e9ce398bc6f3970e1b44c8139bd72ab8e3fd07
felixhelenius <FeliksHelenius@gmail.com>
2026-02-27 12:28:29 +0100

    Build

--- frontend/react/vite.config.ts
+++ frontend/react/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
+
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+	],
+	build: {
+		outDir: "../../dist",
+		emptyOutDir: true,
+	},
+});
